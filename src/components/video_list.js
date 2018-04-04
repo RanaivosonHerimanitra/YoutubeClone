dcde00f29165eeca4b79db00417e0bd209cd250a
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2018-04-04 18:07:07 -0400

    handling component tree construction videoList>videoItem

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import VideoListItem from './components/video_list_item'
+import React, {Component} from 'react'
+import VideoListItem from './video_list_item'
 
 const VideoList = (props) => {
+    const videoItems = props.videos.map( (video)=> {
+        return <VideoListItem key={video.etag} video={video} />
+    })
     return (
         <ul className="col-md-4 list-group">
-        {props.videos.length}
+          {videoItems}
         </ul>
     )
 }
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,1 +1,17 @@
 import React from 'react'
+
+
+const VideoListItem = ({video}) => {
+   console.log(video)
+    return (
+    <li className="list-group-item">Video
+      <div className="video-list media">
+         <div className="media-left">
+         </div>
+
+      </div>
+    </li>
+    )
+}
+
+export default VideoListItem;
